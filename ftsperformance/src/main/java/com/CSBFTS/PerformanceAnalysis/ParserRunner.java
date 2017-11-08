0fed156ccbf21165e4f6d19c8cbc290e67398b9f
bencandell <bsc218@lehigh.edu>
2017-11-07 19:03:39 -0500

    modularized parser runner

--- ftsperformance/src/main/java/com/CSBFTS/PerformanceAnalysis/ParserRunner.java
+++ ftsperformance/src/main/java/com/CSBFTS/PerformanceAnalysis/ParserRunner.java
@@ -1,7 +1,17 @@
 package com.CSBFTS.PerformanceAnalysis;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class ParserRunner {
     public static void main(String[] args) {
+        HashMap<String,Long> map = EventLogParser.parseEventLog();
+
+        Iterator i = map.entrySet().iterator();
+        while(i.hasNext()) {
+            HashMap.Entry pair = (HashMap.Entry)i.next();
+            System.out.println("id: "+pair.getKey() + " timestamp: " + pair.getValue());
+        }
 
     }
 }

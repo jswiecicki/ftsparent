8746b5ace123fb0832f92c0cfb1c7862b681804a
bencandell <bsc218@lehigh.edu>
2017-11-14 17:44:46 -0500

    server config

--- ftsconfig/src/main/java/com/CSBFTS/Config/ServerConfig.java
+++ ftsconfig/src/main/java/com/CSBFTS/Config/ServerConfig.java
@@ -2,5 +2,6 @@ package com.CSBFTS.Config;
 
 public class ServerConfig {
     public final static String ELASTICSEARCH_IP = "35.196.145.84";
-    public final static String KAFKA_IP = "35.202.148.231";
+    public final static String KAFKA_IP = "35.196.124.101:9092";
+    public final static String KAFKA_TOPIC = "test3";
 }
